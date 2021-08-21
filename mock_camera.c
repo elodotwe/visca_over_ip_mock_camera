664de436acf360a620facd5ae2bb0cd859aea77d
Jacob Rau <jarau@mtu.edu>
2021-08-20 23:03:26 -0400

    find and print frames

--- mock_camera.c
+++ mock_camera.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#include "jr_socket.h"
+#include <jr_socket.h>
 
-#include "jr_hex_print.h"
+#include <jr_hex_print.h>
+#include <jr_visca.h>
+#include <string.h>
 
 int main() {
     printf("hello\n");
@@ -19,13 +21,32 @@ int main() {
         return -2;
     }
 
-    int count;
+    int count = 0;
     char buffer[1024];
+    jr_viscaFrame frame;
 
-    while ((count = jr_socket_receive(clientSocket, buffer, 1024)) > 0) {
+    int latestCount;
+    while ((latestCount = jr_socket_receive(clientSocket, buffer + count, 1024 - count)) > 0) {
+        count += latestCount;
         printf("recv: ");
         hex_print(buffer, count);
         printf("\n");
+        int consumed;
+        do {
+            consumed = jr_viscaDataToFrame(buffer, count, &frame);
+            if (consumed) {
+                printf("found %d-byte frame: ", consumed);
+                printf("sender: %d, receiver: %d, data: ", frame.sender, frame.receiver);
+                hex_print(frame.data, frame.dataLength);
+                printf("\n");
+
+                count -= consumed;
+                // Crappy naive buffer management-- move remaining bytes up to buffer[0].
+                // Maybe later we'll replace this with a circular buffer or something.
+                // For now I just want it to work.
+                memmove(buffer, buffer + consumed, count);
+            }
+        } while (consumed);
     }
 
     printf("Connection spun down, terminating.\n");
